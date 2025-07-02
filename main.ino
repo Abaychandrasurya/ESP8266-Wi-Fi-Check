165bc36d8d6042de23a6764f761c6dabb971e9c9
Abaychandrasurya J K <abhayc2274@gmail.com>
2025-07-02 22:23:39 +0530

    Create main.ino

---
+++ main.ino
@@ -0,0 +1,41 @@
+#include <ESP8266WiFi.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_RESET -1
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+// Replace with your WiFi credentials
+const char* ssid = " name ";
+const char* password = " password ";
+
+void setup() {
+  Serial.begin(115200);
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  display.clearDisplay();
+  
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0, 0);
+  display.println("Connecting to WiFi...");
+  display.display();
+
+  WiFi.begin(ssid, password);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  display.clearDisplay();
+  display.setCursor(0, 0);
+  display.println("WiFi connected!");
+  display.println(WiFi.localIP());
+  display.display();
+}
+
+void loop() {
+}
